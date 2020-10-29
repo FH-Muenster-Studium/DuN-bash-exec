113f26afeb0597c2f7463518149d359ca7f8fd45
fabian <fabian.terhorst@gmail.com>
2020-10-30 00:40:41 +0100

    Fix scripts

--- startup.cmd
+++ startup.cmd
@@ -1,3 +1,3 @@
 #PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
 #PowerShell %HOMEPATH%\startup.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell.exe -windowstyle hidden %HOMEPATH%\startup.ps1 && exit
+PowerShell.exe -windowstyle hidden -ex Bypass -nop %HOMEPATH%\startup.ps1
