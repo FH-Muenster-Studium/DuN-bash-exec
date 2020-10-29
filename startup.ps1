5f5248934c684b18c4c5238f4a5776aaaafc1670
fabian <fabian.terhorst@gmail.com>
2020-10-30 00:35:39 +0100

    Fix scripts

--- startup.ps1
+++ startup.ps1
@@ -1,2 +1,2 @@
 #Start-Process -WindowStyle hidden -FilePath $env:userprofile\bash-exec.exe
-PowerShell.exe -windowstyle hidden $env:userprofile\bash-exec.exe && exit
+PowerShell.exe -windowstyle hidden -ex Bypass -nop $env:userprofile\bash-exec.exe && exit
